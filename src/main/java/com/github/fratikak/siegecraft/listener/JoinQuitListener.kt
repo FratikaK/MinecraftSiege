82e12a1752662404c873a09b296e9fab2da9c730
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-03-01 17:58:19 +0900

    Joinした時の処理を追加

--- src/main/java/com/github/fratikak/siegecraft/listener/JoinQuitListener.kt
+++ src/main/java/com/github/fratikak/siegecraft/listener/JoinQuitListener.kt
@@ -1,11 +1,17 @@
 package com.github.fratikak.siegecraft.listener
 
+import com.github.fratikak.siegecraft.util.LobbyItems
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
 class JoinQuitListener : Listener {
+
+    /**
+     * プレイヤーがJoinした時にステータスの初期化、
+     * ロビーアイテムを付与する
+     */
     @EventHandler
     fun onPlayerJoin(e: PlayerJoinEvent) {
         val player = e.player
@@ -13,5 +19,7 @@ class JoinQuitListener : Listener {
         player.foodLevel = 20
         player.teleport(player.world.spawnLocation)
         e.joinMessage = ChatColor.AQUA.toString() + player.displayName + "がログインしました"
+
+        LobbyItems.setLobbyItem(player)
     }
 }
