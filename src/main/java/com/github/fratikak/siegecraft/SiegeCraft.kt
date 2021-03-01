d52aa01c940c405d4294755a79fed2016a05c790
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-03-01 18:00:20 +0900

    リスナの登録

--- src/main/java/com/github/fratikak/siegecraft/SiegeCraft.kt
+++ src/main/java/com/github/fratikak/siegecraft/SiegeCraft.kt
@@ -1,6 +1,7 @@
 package com.github.fratikak.siegecraft
 
 import com.github.fratikak.siegecraft.listener.JoinQuitListener
+import com.github.fratikak.siegecraft.listener.PlayerControlListener
 import com.github.fratikak.siegecraft.listener.SiegeControlListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
@@ -9,6 +10,7 @@ class SiegeCraft : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         Bukkit.getPluginManager().registerEvents(JoinQuitListener(), this)
+        Bukkit.getPluginManager().registerEvents(PlayerControlListener(this), this)
         Bukkit.getPluginManager().registerEvents(SiegeControlListener(), this)
     }
 
