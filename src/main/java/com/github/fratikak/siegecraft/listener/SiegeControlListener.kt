5ab345cad1c49dd9cea129e76d58439b7b37520b
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-03-06 19:31:27 +0900

    teleport処理を追加

--- src/main/java/com/github/fratikak/siegecraft/listener/SiegeControlListener.kt
+++ src/main/java/com/github/fratikak/siegecraft/listener/SiegeControlListener.kt
@@ -2,7 +2,9 @@ package com.github.fratikak.siegecraft.listener
 
 import com.github.fratikak.siegecraft.SiegeManager
 import com.github.fratikak.siegecraft.event.SiegeStartEvent
+import com.github.fratikak.siegecraft.util.GameStage
 import org.bukkit.ChatColor
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
@@ -15,21 +17,29 @@ class SiegeControlListener : Listener {
     fun onSiegeStart(e: SiegeStartEvent) {
 
         //準備Task中であればreturn
-        if (SiegeManager.isPreparation){
+        if (SiegeManager.isPreparation) {
             return
         }
 
         SiegeManager.startMatch()
 
+        //使用ステージ選出(ステージIDをランダムに決める)
+        val stageID = 0  //TODO ステージを増やす度に乱数の範囲を広げる
+        SiegeManager.stageID = stageID
+
         //各チームをそれぞれのスポーンポイントに移動させる
         for (player in SiegeManager.blueTeam) {
-            //TODO テレポート処理
+            //ステージIDに対応したステージにテレポート
+            teleportStageBlue(stageID, player)
+
             player.sendMessage(ChatColor.GOLD.toString() + "--------------------------")
             player.sendMessage(ChatColor.GOLD.toString() + "[勝利条件]：コアの破壊")
             player.sendMessage(ChatColor.GOLD.toString() + "--------------------------")
         }
         for (player in SiegeManager.redTeam) {
-            //TODO テレポート処理
+            //ステージIDに対応したステージにテレポート
+            teleportStageRed(stageID, player)
+
             player.sendMessage(ChatColor.GOLD.toString() + "--------------------------")
             player.sendMessage(ChatColor.GOLD.toString() + "[勝利条件]：残り時間が0になる")
             player.sendMessage(ChatColor.GOLD.toString() + "--------------------------")
@@ -40,5 +50,17 @@ class SiegeControlListener : Listener {
 
         //GameCountDownTaskの開始
 
+    }
+
+    private fun teleportStageBlue(stageId: Int, p: Player) {
+        when (stageId) {
+            0 -> p.teleport(GameStage.APARTMENT.redLocation(p))
+        }
+    }
+
+    private fun teleportStageRed(stageId: Int, p: Player) {
+        when (stageId) {
+            0 -> p.teleport(GameStage.APARTMENT.redLocation(p))
+        }
     }
 }
