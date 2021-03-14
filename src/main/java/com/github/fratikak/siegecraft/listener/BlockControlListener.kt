70d95372c58a38dcd8dece49ad086e2712530ba8
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-03-14 23:55:04 +0900

    ブロック関連の処理を追加

--- src/main/java/com/github/fratikak/siegecraft/listener/BlockControlListener.kt
+++ src/main/java/com/github/fratikak/siegecraft/listener/BlockControlListener.kt
@@ -11,12 +11,26 @@ import org.bukkit.event.block.BlockBreakEvent
 
 class BlockControlListener : Listener {
 
+    /**
+     * 試合中にダイアモンドブロックを破壊した時に
+     * CoreDamageEventをcallします
+     */
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onDiamondBlockDamage(e: BlockBreakEvent) {
 
         if (e.block.type == Material.DIAMOND_BLOCK){
             e.isCancelled = true
             Bukkit.getPluginManager().callEvent(CoreDamageEvent())
+        }
+    }
+
+    /**
+     * opを所持していないプレイヤーのブロック破壊はキャンセルします
+     */
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun onBlockBreak(e:BlockBreakEvent){
+        if(!e.player.isOp){
+            e.isCancelled = true
         }
     }
 }
