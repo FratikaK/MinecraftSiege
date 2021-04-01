b058bcb63329a50f92c194ccd2f0a741a3c1fdd7
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-04-01 17:21:45 +0900

    試合中のリスポーン処理を追加

--- src/main/java/com/github/fratikak/siegecraft/listener/PlayerControlListener.kt
+++ src/main/java/com/github/fratikak/siegecraft/listener/PlayerControlListener.kt
@@ -3,11 +3,16 @@ package com.github.fratikak.siegecraft.listener
 import com.github.fratikak.siegecraft.SiegeManager
 import com.github.fratikak.siegecraft.task.RecoveryStaminaTask
 import com.github.fratikak.siegecraft.task.SprintControlTask
+import com.github.fratikak.siegecraft.util.GameStage
+import org.bukkit.ChatColor
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerRespawnEvent
 import org.bukkit.event.player.PlayerToggleSprintEvent
 import org.bukkit.plugin.Plugin
 
@@ -20,17 +25,39 @@ class PlayerControlListener(private val plugin: Plugin) : Listener {
      * voidダメージを受けた場合は初期スポーン地点へ戻ります
      */
     @EventHandler
-    fun onPlayerDamage(e:EntityDamageEvent){
+    fun onPlayerDamage(e: EntityDamageEvent) {
         //プレイヤーじゃなければreturn
-        if (e.entity.type != EntityType.PLAYER){
+        if (e.entity.type != EntityType.PLAYER) {
             return
         }
-        if (manager.isMatching){
+
+        val player = e.entity as Player
+        if (manager.isMatching) {
             e.isCancelled = true
         }
         //voidダメージならば初期スポーンへ
-        if (e.cause == EntityDamageEvent.DamageCause.VOID){
-            e.entity.teleport(e.entity.world.spawnLocation)
+        if (e.cause == EntityDamageEvent.DamageCause.VOID) {
+            if (!manager.gamePlayers.contains(player)) {
+                e.entity.teleport(e.entity.world.spawnLocation)
+            }
+        }
+    }
+
+    @EventHandler
+    fun onPlayerDeath(e: PlayerDeathEvent) {
+        //TODO 死亡メッセージの編集
+    }
+
+    @EventHandler
+    fun onPlayerRespawn(e: PlayerRespawnEvent) {
+        val player = e.player
+        //TODO 観戦Task実行
+        if (manager.isMatching) {
+            if (manager.blueTeam.contains(player)) {
+                e.respawnLocation = manager.blueLocation!!
+            }else if (manager.redTeam.contains(player)){
+                e.respawnLocation = manager.redLocation!!
+            }
         }
     }
 
@@ -38,11 +65,11 @@ class PlayerControlListener(private val plugin: Plugin) : Listener {
      * プレイヤーが走ったり、歩いたりでスタミナが増減します
      */
     @EventHandler
-    fun onStaminaControl(e:PlayerToggleSprintEvent){
-        if (e.isSprinting){
-            SprintControlTask(e.player).runTaskTimer(plugin,0,10)
-        }else{
-            RecoveryStaminaTask(e.player).runTaskTimer(plugin,0,5)
+    fun onStaminaControl(e: PlayerToggleSprintEvent) {
+        if (e.isSprinting) {
+            SprintControlTask(e.player).runTaskTimer(plugin, 0, 10)
+        } else {
+            RecoveryStaminaTask(e.player).runTaskTimer(plugin, 0, 5)
         }
     }
 
@@ -50,7 +77,7 @@ class PlayerControlListener(private val plugin: Plugin) : Listener {
      * 通常の食料ゲージ減少はキャンセルします
      */
     @EventHandler
-    fun onFoodLevel(e:FoodLevelChangeEvent){
+    fun onFoodLevel(e: FoodLevelChangeEvent) {
         e.isCancelled = true
     }
 }
