36b8e561309ec710b997a6095d5c8b02e6505b55
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-04-02 18:31:32 +0900

    必要なコンストラクタのパラメータを追加

--- src/main/java/com/github/fratikak/siegecraft/task/GameCountDownTask.kt
+++ src/main/java/com/github/fratikak/siegecraft/task/GameCountDownTask.kt
@@ -3,13 +3,14 @@ package com.github.fratikak.siegecraft.task
 import com.github.fratikak.siegecraft.SiegeManager
 import com.github.fratikak.siegecraft.util.ScoreBoardSystem
 import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
 import org.bukkit.scheduler.BukkitRunnable
 
 /**
  * ゲームの残り時間カウント、スコアボード更新など
  * １秒毎に更新したい内容を反映するタスク
  */
-class GameCountDownTask : BukkitRunnable() {
+class GameCountDownTask(private val plugin: Plugin) : BukkitRunnable() {
 
     override fun run() {
 
@@ -26,6 +27,15 @@ class GameCountDownTask : BukkitRunnable() {
         //スコアボード更新
         for (player in Bukkit.getOnlinePlayers()){
             ScoreBoardSystem(player).updateScoreBoard()
+        }
+
+        if (SiegeManager.timeLeft <= 0){
+            SiegeManager.isMatching = false
+            SiegeManager.isFinish = true
+            //終了Taskを呼び出す
+            GameFinishTask().runTaskTimer(plugin,0,20)
+            cancel()
+            return
         }
 
         if (SiegeManager.coreHealth <= 0){
