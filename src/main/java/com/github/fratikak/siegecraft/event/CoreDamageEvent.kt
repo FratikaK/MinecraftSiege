95deb0d8739f9094fc90c9ff91d82790646c2201
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-04-05 15:05:08 +0900

    コンストラクタのパラメータを追加

--- src/main/java/com/github/fratikak/siegecraft/event/CoreDamageEvent.kt
+++ src/main/java/com/github/fratikak/siegecraft/event/CoreDamageEvent.kt
@@ -1,15 +1,17 @@
 package com.github.fratikak.siegecraft.event
 
+import org.bukkit.entity.Player
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
 /**
  * コアにダメージが入った場合に呼び出されるEvent
  */
-class CoreDamageEvent : Event() {
+class CoreDamageEvent(val player: Player) : Event() {
     override fun getHandlers(): HandlerList {
         return HANDLERS
     }
+
     companion object {
         private val HANDLERS = HandlerList()
 
